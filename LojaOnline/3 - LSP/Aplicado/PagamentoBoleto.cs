5a8e2d8a4fffc413bdcea02683b34995ad067254
Rebeca Albuquerque <rebecaalbuquerke@gmail.com>
2021-06-07 09:27:06 -0300

    Correção do LSP

---
+++ LojaOnline/3 - LSP/Aplicado/PagamentoBoleto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoSOLID._3___LSP.Aplicado
+{
+    public interface IFormaDePagamento
+    {
+        double ValorDoPagamento { get; }
+    }
+
+    public class PagamentoBoleto : IFormaDePagamento
+    {
+        public double ValorDoPagamento => ValorDoPagamento * 1;
+    }
+
+    public abstract class PagamentoDinheiro : IFormaDePagamento
+    {
+        public double ValorDoPagamento => ValorDoPagamento * 0.95;
+    }
+}
---
+++ LojaOnline/3 - LSP/Não Aplicado/PagamentoBoleto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoSOLID._3___LSP.Não_Aplicado
+{
+    public abstract class PagamentoBoleto
+    {
+        public abstract double ValorDoPagamento { get; set; }
+    }
+
+    public abstract class PagamentoDinheiro : PagamentoBoleto
+    {
+        public override double ValorDoPagamento
+        {
+            get { return ValorDoPagamento; }
+            set { ValorDoPagamento = ValorDoPagamento * 0.95; }
+        }
+    }
+}
